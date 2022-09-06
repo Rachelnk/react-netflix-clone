5e68c15947b419172510891d2a717f810bef946a
Rachel Kiarie <rachelnnk94@gmail.com>
2022-09-06 16:22:30 +0300

    Add styling for the body; margin, padding, text-decoration.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState, useEffect } from 'react';
+import MovieBox from './MovieBox';
 import './App.css';
 
+const API_URL="https://api.themoviedb.org/3/movie/550?api_key=3114892bac012791fbd2d879d1d3a7d1";
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+      fetch(API_URL)
+      .then((res)=>res.json())
+      .then(data=>{
+        console.log(data);
+        setMovies(data.results);
+      })
+  }, [])
   return (
     <>
-    <h1>Hi</h1>
+    <div>
+      
+      {movies.map((movieReq)=>
+      <MovieBox key={movieReq.id} {...movieReq}/>)}
+
+    </div>
     </>
   );
 }
