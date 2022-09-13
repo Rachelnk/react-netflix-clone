740095f190f9b8c046c496b49fa05d9436bc2947
Rachel Kiarie <rachelnnk94@gmail.com>
2022-09-13 12:43:22 +0300

    Add modal to show movie details.

--- src/App.js
+++ src/App.js
@@ -2,31 +2,45 @@ import { useState, useEffect } from 'react';
 import MovieBox from './MovieBox';
 import './App.css';
 
-const API_URL="https://api.themoviedb.org/3/movie/550?api_key=3114892bac012791fbd2d879d1d3a7d1";
+
 function App() {
+  const API_URL='https://api.themoviedb.org/3/movie/550?api_key=3114892bac012791fbd2d879d1d3a7d1';
 
   const [movies, setMovies] = useState([]);
   useEffect(() => {
       fetch(API_URL)
-      .then((res)=>res.json())
-      .then(data=>{
+      .then((res) =>res.json())
+      .then(data =>{
         console.log(data);
         setMovies(data.results);
       })
-  }, [])
+  }, []);
+ 
+
+  if(movies) {
   return (
     <>
     <div className='container'>
       <div className='grid'>
-        {/* {movies.map((movieReq)=>
-      <MovieBox key={movieReq.id} {...movieReq}/>)} */}
-      </div>
-      
-      
-
+        <h1>Hi</h1>
+        
+        {movies.map((movieReq) => (
+          <MovieBox key={movieReq.id} {...movieReq}/>))        
+      }
+      </div>       
     </div>
     </>
+    
   );
+}
+else {
+  return(
+    <p>No films or TV shows found</p>)
+}
+
+
+    
+
 }
 
 export default App;
--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,11 +1,28 @@
+import { Modal } from "bootstrap";
 const API_IMG="https://image.tmdb.org/t/p/w500/";
-const MovieBox =({title, poster_path, vote_average, release_date, overview}) =>{
+
+ function MovieBox (title, poster_path, vote_average, release_date, overview){
   return(
-    <div>
-      <h1>{title}</h1>
-      <img src={API_IMG+poster_path} alt='movie poster'/>
-      <p>{overview}</p>
-    </div>
+    <>
+      
+      <div className="card text-center bg-secondary mb-3" >
+        <div className="card-body">
+        <img src={API_IMG+poster_path} alt='movie poster'className="card-img-top"/>
+          <h1> {title}</h1>     
+            <p>{overview}</p>
+        </div>
+        <div className="card-body">
+          <button type="button" className="btn btn-dark">View More </button> 
+            <Modal>
+              <Modal.Header closeButton>
+                <Modal.Title></Modal.Title>
+
+              </Modal.Header>
+              <Modal.Body></Modal.Body>
+              </Modal>    
+        </div>      
+      </div>
+    </>
   )
 }
 export default MovieBox;
