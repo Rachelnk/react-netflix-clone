2d09b60c6e4213de190ee62237b1c94c527ce58c
Rachel Kiarie <rachelnnk94@gmail.com>
2022-09-18 18:54:16 +0300

    Add function to search for movie.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import MovieBox from './MovieBox';
 import './App.css';
-import { Navbar, Container } from 'react-bootstrap';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 
 function App() {
-  const API_URL='https://api.themoviedb.org/3/movie/550?api_key=3114892bac012791fbd2d879d1d3a7d1';
+  const API_URL='https://api.themoviedb.org/3/movie/popular?api_key=3114892bac012791fbd2d879d1d3a7d1';
+  const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=3114892bac012791fbd2d879d1d3a7d1&query";
+
 
   const [movies, setMovies] = useState([]);
   useEffect(() => {
@@ -17,21 +19,61 @@ function App() {
       })
   }, []);
  
+const searchMovie = async(e)=>{
+    e.preventDefault();
+    console.log("searching");
+    try{
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=3114892bac012791fbd2d879d1d3a7d1&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e){
+      console.log(e);
+    }
 
+}
   if(movies) {
   return (
-    <>
-    <Navbar bg="dark" expand="lg" variant="dark">
-      <Container fluid>
-        <Navbar.Brand href="">Netflix Clone</Navbar.Brand>
-      </Container>
-    </Navbar>
+    <>    
+    <nav className='navbar navbar-expand-lg navbar-light sticky-top' style ={{backgroundColor: 'black'}}>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/home" style={{color: '#e63c35', fontSize: '30px'}}>Netflix Clone </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            
+                    <ul className='navbar-nav mx-auto >'> 
+                    <li className='nav-item' style={{fontSize: '30px', padding: '0 20px', paddingTop: '10px !important', color: 'white'}}><a className="nav-link" href="/home">Trending</a></li>
+                    
+                    <li className='nav-item' style={{fontSize: '30px', padding: '0 20px', paddingTop: '10px !important'}}><a className="nav-link" href="#about">
+                    <Form className="d-flex" onSubmit={searchMovie}>
+              <FormControl 
+              type="search" 
+              placeholder="Movie Search" 
+              className="me-2" 
+              aria-label="search"
+              name=""/>
+              <Button variant="secondary" type="submit">Search</Button>
+          </Form>
+                      
+                      </a></li>            
+                     
+                    </ul>
+                     
+                 
+                 
+            </div>    
+          </div>       
+           
+     </nav>
     <div className='container'>
-      <div className='grid'>
-        <h1>Hi</h1>
+      <div className='grid'>      
         
-        {movies.map((movieReq) => (
-          <MovieBox key={movieReq.id} {...movieReq}/>))        
+        {movies.map((movieReq) => 
+          <MovieBox key={movieReq.id} {...movieReq}/>)        
       }
       </div>       
     </div>
@@ -42,12 +84,9 @@ function App() {
 else {
   return(
     <div>
-    {/* <p>No films or TV shows found</p> */}
-    <Navbar bg="dark" expand="lg" variant="dark">
-      <Container fluid>
-        <Navbar.Brand href="" style={{color: '#e63c35', fontSize: '30px'}}>Netflix Clone</Navbar.Brand>
-      </Container>
-    </Navbar>
+     
+    <p>No films or TV shows found</p>
+    
     </div>)
 }
 
